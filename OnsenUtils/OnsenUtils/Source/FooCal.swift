1cee70da9ba24a2374397de921925c7daab61a74
Thanaphat Suwannikornkul <onsensei@gmail.com>
2017-11-01 14:20:47 +0700

    initial with test class

---
+++ OnsenUtils/OnsenUtils/Source/FooCal.swift
@@ -0,0 +1,16 @@
+//
+//  FooCal.swift
+//  OnsenUtils
+//
+//  Created by Thanaphat Suwannikornkul on 01/11/60 BE.
+//  Copyright © 2560 Thanaphat Suwannikornkul. All rights reserved.
+//
+
+import UIKit
+
+public class FooCal: NSObject {
+    public func print(msg:String)
+    {
+        NSLog("test msg = %@", msg)
+    }
+}
